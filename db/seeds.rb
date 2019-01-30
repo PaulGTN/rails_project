706749c7b43bff486bf0d7901cdee22e5599d5a5
Paul.Graton <paulgtn@hotmail.fr>
2019-01-30 18:21:06 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-User.destroy_all
-Article.destroy_all
-Category.destroy_all
-Commentaire.destroy_all
+#User.destroy_all
+#Article.destroy_all
+#Category.destroy_all
+#Commentaire.destroy_all
 
 10.times do 
   user = User.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.email)
 end
 
-10.times do 
-  article = Article.create!(title: Faker::Food.unique.dish, content: Faker::Food.unique.description, user_id: (rand(10) + 1), category_id: (rand(5) + 1))
-end
-
 5.times do 
   category = Category.create!(name: Faker::Food.unique.vegetables)
 end 
+
+10.times do 
+  article = Article.create!(title: Faker::Food.unique.dish, content: Faker::Food.unique.description, user_id: (rand(10) + 1), category_id: (rand(5) + 1))
+end
 
 15.times do 
   commentaire = Commentaire.create!(content: Faker::Friends.unique.quote, user_id: (rand(10) + 1), article_id: (rand(10) + 1))
