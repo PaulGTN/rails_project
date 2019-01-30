542fb98a1404585ca53f314fcfd994dfcfb11f00
Paul.Graton <paulgtn@hotmail.fr>
2019-01-30 17:37:47 +0100

    Dur dur

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :commentaires
+  has_many :likes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-#User.destroy_all
-#Article.destroy_all
-#Category.destroy_all
-#Commentaire.destroy_all
+User.destroy_all
+Article.destroy_all
+Category.destroy_all
+Commentaire.destroy_all
 
 10.times do 
   user = User.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.email)
 end
 
 10.times do 
-  article = Article.create!(user_id: rand(10) + 1, category_id: rand(10) + 1, title: Faker::Food.unique.dish, content: Faker::Food.unique.description)
+  article = Article.create!(title: Faker::Food.unique.dish, content: Faker::Food.unique.description, user_id: (rand(10) + 1), category_id: (rand(5) + 1))
 end
 
 5.times do 
   category = Category.create!(name: Faker::Food.unique.vegetables)
 end 
 
-#15.times do 
-#  commentaire = Commentaire.create!(user_id: rand(10) + 1, article_id: rand (10) + 1, content: Faker::Friends.unique.quote)
-#end 
+15.times do 
+  commentaire = Commentaire.create!(content: Faker::Friends.unique.quote, user_id: (rand(10) + 1), article_id: (rand(10) + 1))
+end 
+
+15.times do
+  like = Like.create!(user_id: (rand(10) + 1), article_id: (rand(10) + 1))
+end
+
